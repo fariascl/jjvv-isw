4509360775bc7050878f08ca511a5ff4c252cbfc
Benjax14 <benjamin.contreras1901@alumnos.ubiobio.cl>
2021-10-13 20:10:28 -0300

    Agrega verdescripcion y fix de referencias

--- verdescripcion.php
+++ verdescripcion.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ver comunidades</title>
+    <title>Descripcion</title>
     <link rel="stylesheet" href="static/css/menu.css">
-    <link rel="stylesheet" href="static/css/vercomunidad.css">
+    <link rel="stylesheet" href="static/css/verdescripcion.css">
 </head>
-
 <body>
     <div class="container">
         <div class="menu">
@@ -36,7 +34,7 @@
         <div class="main">
             <div class="center">
                 <form action="">
-                    <?php
+                    <!-- <?php
                     include('include/functions.php');
                     $row = get_comunidad();
                     foreach ($row as $comunidad){
@@ -46,7 +44,12 @@
                             <a href="#" class="button">Descripción</a>
                         </div>';
                     }
-                    ?>
+                    ?>-->
+                    <p class="tittle">Junta de Vecinos X</p>
+                    <div>
+                        <textarea class="input textarea" cols="30" rows="10" placeholder="Esto es una comunidad promedio" readonly><?php echo $comunidad[2]; ?> </textarea>
+                    </div>
+                    <a class="button" href="vercomunidad.php">Volver</a>
                 </form>
             </div>
         </div>
