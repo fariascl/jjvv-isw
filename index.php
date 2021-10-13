16740c66cf8cca0b0cd643c944b8b1f65c21386a
fariascl <farias@8loop.cl>
2021-10-13 15:51:40 -0300

    se agrega index

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="static/css/contacto.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="menu">
+            <div class="logo">
+                <a href="#">J.J.V.V</a>
+            </div>
+            <div class="action">
+                <a href="#">Home</a>
+                <a href="reuniones.php">Reuniones</a>
+                <a href="contacto.php" class="active">Contacto</a>
+                <a href="actas.php">Actas</a>
+                <a href="login.php">Iniciar Sesión</a>
+                <a href="register.html">Registro</a>
+            </div>
+        </div>
+        <div class="main">
+            
+        </div>
+    </div>
+</body>
+
+</html>
