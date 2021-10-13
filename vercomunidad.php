2f68b4d5ee5d0d81534861de8f87d3ffc6399b26
fariascl <farias@8loop.cl>
2021-10-13 20:45:20 -0300

    comunidad fixed

--- vercomunidad.php
+++ vercomunidad.php
@@ -43,7 +43,7 @@
                         echo '
                         <div class="block">
                             <p>' .$comunidad[1]. '</p>
-                            <a href="verdescripcion.php" class="button">Descripción</a>
+                            <a href="verdescripcion.php?id='.$comunidad[0].'" class="button">Descripción</a>
                         </div>';
                     }
                     ?>
