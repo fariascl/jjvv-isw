2f8d45a42c8f30058d14e329e1f7e7da299a000b
fariascl <farias@8loop.cl>
2021-10-15 00:45:52 -0300

    agregando login

--- include/session.php
+++ include/session.php
@@ -2,12 +2,13 @@
 
 function check_session(){
     if (!isset($_SESSION)){
-        $logged_in = $_SESSION['logged_in'];
-        if (empty($logged_in)){
-            return true;
-        }
-        return false;
+        session_start();
     }
+    $logged_in = $_SESSION['logged_in'];
+    if (empty($logged_in)){
+        return true;
+    }
+    return false;
 }
 
 
