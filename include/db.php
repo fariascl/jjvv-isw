f22bbdbc667b24ce02dc05924bda716401bc5914
Alejandro <49617228+fariascl@users.noreply.github.com>
2021-10-29 00:11:01 -0300

    Seteo de default charset base de datos

--- include/db.php
+++ include/db.php
@@ -5,6 +5,7 @@ $dbname = 'isw';
 $host = 'localhost';
 
 $conn = new mysqli($host, $userdb, $passdb, $dbname);
+$conn->set_charset("utf8"); // Se setea default_charset para la base de datos
 if ($conn->connect_error){
     die('Error al conectar al servidor SQL' . $conn->connect_error);
 }
